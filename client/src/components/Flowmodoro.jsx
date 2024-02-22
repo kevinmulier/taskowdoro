56e21dbebdff1e9bf2db584d38916c4851048e65
kevinmulier <kevin.mulier@outlook.fr>
2024-02-22 21:27:16 +0100

    refactor to improve readibility

--- client/src/components/Flowmodoro.jsx
+++ client/src/components/Flowmodoro.jsx
@@ -2,78 +2,54 @@ import { Play, Square } from 'lucide-react';
 import { useEffect, useState } from 'react';
 
 const Flowmodoro = () => {
-  const [focusTime, setFocusTime] = useState({
-    time: 0,
-    formatedTime: '00:00',
-  });
-  const [isFocused, setIsFocused] = useState(false);
-
-  const [restTime, setRestTime] = useState({
-    time: 0,
-    formatedTime: '00:00',
-  });
-  const [isResting, setIsResting] = useState(false);
+  const [time, setTime] = useState(0);
+  const [formattedTime, setFormattedTime] = useState('00:00');
+  const [mode, setMode] = useState(null);
 
   useEffect(() => {
     let intervalId = null;
 
-    if (isFocused) {
-      setFocusTime({ time: 0, formatedTime: '00:00' });
-
-      intervalId = setInterval(() => {
-        setFocusTime((v) => ({
-          time: v.time + 1,
-          formatedTime: formatTime((v.time + 1) * 1000),
-        }));
-      }, 1000);
-    }
-
-    if (isResting) {
-      setFocusTime({ time: 0, formatedTime: '00:00' });
-
-      intervalId = setInterval(() => {
-        setRestTime((v) => {
-          const newTime = v.time - 1;
-          if (newTime <= 0) {
-            clearInterval(intervalId);
-            setIsResting(false);
-            return {
-              time: 0,
-              formatedTime: '00:00',
-            };
-          }
-          return {
-            time: newTime,
-            formatedTime: formatTime(newTime * 1000),
-          };
-        });
-      }, 1000);
-    }
-
-    return () => {
-      clearInterval(intervalId);
+    const updateTime = (increment) => {
+      setTime((prevTime) => {
+        const newTime = increment ? prevTime + 1 : prevTime - 1;
+        setFormattedTime(formatTime(newTime * 1000));
+        return newTime;
+      });
     };
-  }, [isFocused, isResting]);
 
-  const handleFocus = () => {
-    if (isFocused) {
-      setIsFocused(false);
-      setRestTime({
-        time: Math.floor(focusTime.time / 5),
-        formatedTime: formatTime((focusTime.time / 5) * 1000),
-      });
-      return;
-    }
-    if (isResting) {
-      setIsResting(false);
-      setRestTime({
-        time: 0,
-        formatedTime: '00:00',
-      });
-      return;
+    if (mode === 'focus') {
+      intervalId = setInterval(() => updateTime(true), 1000);
+    } else if (mode === 'rest') {
+      intervalId = setInterval(() => {
+        updateTime(false);
+        if (time <= 1) {
+          clearInterval(intervalId);
+          setMode(null);
+        }
+      }, 1000);
     }
 
-    restTime.time ? setIsResting(true) : setIsFocused(true);
+    return () => clearInterval(intervalId);
+  }, [mode, time]);
+
+  const toggleMode = () => {
+    setMode((prevMode) => {
+      if (prevMode === 'focus') {
+        const newRestTime = Math.floor(time / 5);
+        if (newRestTime > 0) {
+          setTime(newRestTime);
+          setFormattedTime(formatTime(newRestTime * 1000));
+          return 'rest';
+        }
+        setTime(0);
+        setFormattedTime('00:00');
+        return null;
+      } else {
+        setTime(0);
+        setFormattedTime('00:00');
+        return 'focus';
+      }
+    });
   };
 
   const formatTime = (time) => {
@@ -86,24 +62,16 @@ const Flowmodoro = () => {
   return (
     <section className="flex flex-col items-center justify-center max-w-xl gap-3 p-5 mx-auto rounded-lg bg-base-300">
       <h1 className="text-xl font-bold">Flowmodoro</h1>
-      {!restTime.time ? (
-        <div className="flex flex-col items-center justify-center mx-auto">
-          <span>{focusTime.formatedTime}</span>
-          <span>Focus Time</span>
-        </div>
-      ) : (
-        <div className="flex flex-col items-center justify-center mx-auto">
-          <span>{restTime.formatedTime}</span>
-          <span>Break Time</span>
-        </div>
-      )}
-
+      <div className="flex flex-col items-center justify-center mx-auto">
+        <span>{formattedTime}</span>
+        <span>{mode == 'rest' ? 'Break Time' : 'Focus Time'}</span>
+      </div>
       <div className="flex gap-2 max-sm:flex-col">
         <button
           className="btn btn-square btn-outline"
-          onClick={handleFocus}>
-          {(isResting || isFocused) && <Square />}
-          {!isFocused && !isResting && <Play />}
+          onClick={toggleMode}>
+          {mode && <Square />}
+          {!mode && <Play />}
         </button>
       </div>
     </section>
