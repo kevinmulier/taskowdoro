adb8b7e69ab2a7cd62b6350b51f82d0db4668bc0
kevinmulier <kevin.mulier@outlook.fr>
2024-02-23 16:03:42 +0100

    add break ratio + comments

--- client/src/stores/useFlowStore.js
+++ client/src/stores/useFlowStore.js
@@ -12,13 +12,15 @@ const useFlowStore = create((set) => ({
     '--size': '12rem',
     '--thickness': '1rem',
   },
+  focusBreakRatio: 5,
 
   // Action
   toggleMode: () => {
     set((state) => {
       if (state.mode === 'focus') {
-        const newRestTime = Math.floor(state.time / 5);
+        const newRestTime = Math.floor(state.time / state.focusBreakRatio);
 
+        // if mode === focus and new ratio time > 0, rest mode
         if (newRestTime > 0) {
           return {
             time: newRestTime,
@@ -28,10 +30,12 @@ const useFlowStore = create((set) => ({
           };
         }
 
+        // if mode === focus and new ratio time === 0, reset everything
         state.resetTimeAndUI();
         return { mode: null };
       }
 
+      // else, reset time/UI and toggle mode accordingly
       state.resetTimeAndUI();
       return state.mode === 'rest' ? { mode: null } : { mode: 'focus' };
     });
@@ -67,7 +71,12 @@ const useFlowStore = create((set) => ({
       const newTime = increment
         ? state.time + 0.01
         : Math.max(state.time - 0.01, 0);
-      const timeInMilliseconds = newTime > 0 ? Math.ceil(newTime) * 1000 : 0;
+      const timeInMilliseconds =
+        newTime > 0
+          ? increment
+            ? Math.floor(newTime) * 1000
+            : Math.ceil(newTime) * 1000
+          : 0;
       return { time: newTime, formattedTime: formatTime(timeInMilliseconds) };
     });
   },
