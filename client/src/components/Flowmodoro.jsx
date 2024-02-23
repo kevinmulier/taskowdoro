dc614fe8ad6a24437d4dda9997cc802028fefb3f
kevinmulier <kevin.mulier@outlook.fr>
2024-02-23 01:02:11 +0100

    complete basic logic for flowmodoro + refactor for improved readibility

--- client/src/components/Flowmodoro.jsx
+++ client/src/components/Flowmodoro.jsx
@@ -1,54 +1,80 @@
-import { Play, Square } from 'lucide-react';
+import { Play, Settings, Square } from 'lucide-react';
 import { useEffect, useState } from 'react';
 
 const Flowmodoro = () => {
   const [time, setTime] = useState(0);
+  const [savedTime, setSavedTime] = useState(0);
   const [formattedTime, setFormattedTime] = useState('00:00');
   const [mode, setMode] = useState(null);
+  const [progressStyle, setProgressStyle] = useState({
+    '--value': '100',
+    '--size': '12rem',
+    '--thickness': '1rem',
+  });
 
   useEffect(() => {
-    let intervalId = null;
-
     const updateTime = (increment) => {
       setTime((prevTime) => {
-        const newTime = increment ? prevTime + 1 : prevTime - 1;
-        setFormattedTime(formatTime(newTime * 1000));
+        const newTime = increment ? prevTime + 0.01 : prevTime - 0.01;
+        setFormattedTime(formatTime(Math.ceil(newTime) * 1000));
         return newTime;
       });
     };
 
-    if (mode === 'focus') {
-      intervalId = setInterval(() => updateTime(true), 1000);
-    } else if (mode === 'rest') {
-      intervalId = setInterval(() => {
-        updateTime(false);
+    const transitionToEndOfRest = () => {
+      setMode(null);
+      setProgressStyle((currentStyle) => {
+        return { ...currentStyle, '--value': '100' };
+      });
+    };
+
+    if (!['focus', 'rest'].includes(mode)) return;
+
+    const intervalId = setInterval(() => {
+      updateTime(mode === 'focus');
+
+      if (mode === 'rest') {
+        const progress = (((time - 1) / savedTime) * 100).toString();
+        setProgressStyle((currentStyle) => {
+          return { ...currentStyle, '--value': progress };
+        });
+
         if (time <= 1) {
           clearInterval(intervalId);
-          setMode(null);
+          transitionToEndOfRest();
         }
-      }, 1000);
-    }
+      }
+    }, 10);
 
     return () => clearInterval(intervalId);
-  }, [mode, time]);
+  }, [mode, time, savedTime]);
 
   const toggleMode = () => {
     setMode((prevMode) => {
+      const resetTimeAndUI = () => {
+        setTime(0);
+        setFormattedTime('00:00');
+        setProgressStyle((currentStyle) => {
+          return { ...currentStyle, '--value': '100' };
+        });
+      };
+
       if (prevMode === 'focus') {
-        const newRestTime = Math.floor(time / 5);
+        const newRestTime = Math.floor(time * 5);
+
         if (newRestTime > 0) {
           setTime(newRestTime);
+          setSavedTime(newRestTime);
           setFormattedTime(formatTime(newRestTime * 1000));
           return 'rest';
         }
-        setTime(0);
-        setFormattedTime('00:00');
+
+        resetTimeAndUI();
         return null;
-      } else {
-        setTime(0);
-        setFormattedTime('00:00');
-        return 'focus';
       }
+
+      resetTimeAndUI();
+      return prevMode === 'rest' ? null : 'focus';
     });
   };
 
@@ -60,18 +86,24 @@ const Flowmodoro = () => {
   };
 
   return (
-    <section className="flex flex-col items-center justify-center max-w-xl gap-3 p-5 mx-auto rounded-lg bg-base-300">
-      <h1 className="text-xl font-bold">Flowmodoro</h1>
-      <div className="flex flex-col items-center justify-center mx-auto">
+    <section className="flex flex-col items-center justify-center w-full max-w-xl gap-5 p-5 mx-auto rounded-lg bg-base-300">
+      <h1 className="text-xl font-bold">Flow</h1>
+      <div
+        className="flex flex-col items-center justify-center mx-auto radial-progress"
+        style={progressStyle}
+        role="progressbar">
         <span>{formattedTime}</span>
         <span>{mode == 'rest' ? 'Break Time' : 'Focus Time'}</span>
       </div>
       <div className="flex gap-2 max-sm:flex-col">
         <button
-          className="btn btn-square btn-outline"
+          className="btn btn-circle btn-outline"
           onClick={toggleMode}>
           {mode && <Square />}
           {!mode && <Play />}
+        </button>
+        <button className="btn btn-circle btn-outline">
+          <Settings />
         </button>
       </div>
     </section>
