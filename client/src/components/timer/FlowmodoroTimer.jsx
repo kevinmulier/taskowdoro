2a650898f4369d64762e14378ea6c1437cac810b
kevinmulier <kevin.mulier@outlook.fr>
2024-02-25 22:16:54 +0100

    improve timer style

--- client/src/components/timer/FlowmodoroTimer.jsx
+++ client/src/components/timer/FlowmodoroTimer.jsx
@@ -16,11 +16,13 @@ const FlowmodoroTimer = () => {
         className="mx-auto radial-progress"
         style={progressStyle}
         role="progressbar">
-        {!mode && <Play />}
+        {!mode && <Play size={50} />}
         {mode && (
-          <div className="flex flex-col items-center justify-center">
-            <span>{formattedTime}</span>
-            <span>{mode == 'rest' ? 'Break Time' : 'Focus Time'}</span>
+          <div className="flex flex-col items-center justify-center gap-1">
+            <span className="font-mono text-xl">{formattedTime}</span>
+            <span className="text-xl font-bold uppercase">
+              {mode == 'rest' ? 'Break' : 'Focus'}
+            </span>
           </div>
         )}
       </div>
