4a6a46bd7f172e30538f04d263fd9d4f78f9901b
kevinmulier <kevin.mulier@outlook.fr>
2024-02-25 20:40:17 +0100

    import timer and buttons components

--- client/src/components/Flowmodoro.jsx
+++ client/src/components/Flowmodoro.jsx
@@ -1,15 +1,13 @@
-import { Play, Settings, Square } from 'lucide-react';
 import { useEffect } from 'react';
 import useFlowStore from '../stores/useFlowStore';
+import FlowmodoroButtons from './buttons/FlowmodoroButtons';
+import FlowmodoroTimer from './timer/FlowmodoroTimer';
 
 const Flowmodoro = () => {
   const mode = useFlowStore((state) => state.mode);
   const time = useFlowStore((state) => state.time);
   const savedTime = useFlowStore((state) => state.savedTime);
-  const formattedTime = useFlowStore((state) => state.formattedTime);
-  const progressStyle = useFlowStore((state) => state.progressStyle);
 
-  const toggleMode = useFlowStore((state) => state.toggleMode);
   const updateTime = useFlowStore((state) => state.updateTime);
   const updateProgress = useFlowStore((state) => state.updateProgress);
   const resetTimeAndUI = useFlowStore((state) => state.resetTimeAndUI);
@@ -36,24 +34,8 @@ const Flowmodoro = () => {
   return (
     <section className="flex flex-col items-center justify-center w-full max-w-xl gap-5 p-5 mx-auto rounded-lg bg-base-300">
       <h1 className="text-xl font-bold">Flow</h1>
-      <div
-        className="flex flex-col items-center justify-center mx-auto radial-progress"
-        style={progressStyle}
-        role="progressbar">
-        <span>{formattedTime}</span>
-        <span>{mode == 'rest' ? 'Break Time' : 'Focus Time'}</span>
-      </div>
-      <div className="flex gap-2">
-        <button
-          className="btn btn-circle btn-outline"
-          onClick={toggleMode}>
-          {mode && <Square />}
-          {!mode && <Play />}
-        </button>
-        <button className="btn btn-circle btn-outline">
-          <Settings />
-        </button>
-      </div>
+      <FlowmodoroTimer />
+      <FlowmodoroButtons />
     </section>
   );
 };
