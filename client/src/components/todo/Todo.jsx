660555ddffdfdf3efc8712e138d4867eef09c8ad
kevinmulier <kevin.mulier@outlook.fr>
2024-02-27 23:18:02 +0100

    improve padding for mobile display

--- client/src/components/todo/Todo.jsx
+++ client/src/components/todo/Todo.jsx
@@ -6,7 +6,7 @@ const Todo = () => {
   const tasks = useTaskStore((state) => state.tasks);
 
   return (
-    <div className="flex flex-col items-center justify-center w-full gap-5 p-8 mx-auto rounded-lg bg-base-300">
+    <div className="flex flex-col items-center justify-center w-full gap-5 px-2 py-8 mx-auto rounded-lg sm:px-8 bg-base-300">
       <h2 className="text-3xl font-bold">Todo</h2>
       {tasks.length > 0 && <TodoTasksList />}
       <TodoForm />
